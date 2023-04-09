362b6aecfd7f4954f9f42912858e97f454024bb7
beebee1 <dai.linh@northeastern.edu>
2023-04-09 15:52:41 -0700

    move mongoose.connect to the bottom

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://127.0.0.1:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -14,3 +11,6 @@ HelloController(app);
 UserController(app);
 TuitsController(app);
 app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
